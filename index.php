f73799d05e19e4bbe7b5ea6c8735d5aac78c8cfe
Heather Pekrul <heather.pekrul@gmail.com>
2019-11-24 21:32:21 -0600

    Base files for landing page

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Heather Pekrul</title>
+    <link href="https://fonts.googleapis.com/css?family=Fira+Mono&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    <main>
+      <h1>Heather Pekrul</h1>
+
+      <ul class="properties">
+        <li>
+          <a href="http://heather.dev">
+            Professional
+          </a>
+        </li>
+        <li>
+          <a href="https://deaexmachina.io">
+            Gaming
+          </a>
+        </li>
+      </ul>
+
+      <footer>
+        &copy; Copyright <?php echo date('Y'); ?> Heather Pekrul. All rights reserved.
+      </footer>
+    </main>
+  </body>
+</html>
